544fdce158ae9b01aa76242cd84988f4a76cbb60
Barry <barrym07@gmail.com>
2021-11-17 12:39:08 -0700

    mocked appList state

--- src/AppContext.js
+++ src/AppContext.js
@@ -5,72 +5,80 @@ import { useState, useEffect, createContext } from "react";
 
 export const AppContext = createContext(null);
 
-function AppProvider({children}) {
+function AppProvider({ children }) {
 
-//   set initial states here:
-//   const [appList, setAppList] = useState([]); //full list of Apps in database (this is an array of objects of all apps)
-//   const [activeApps, setActiveApps] = useState([]) //set based on token on load (this is an array including each app object that is active)
-//   const [isAdmin, setIsAdmin] = useState([])
-//   const [searchTerm, setSearchTerm] = useState([])
-// a favorites or MyApp state?
+  //   set initial states here:
+  const [appList, setAppList] = useState([
+    { name: `MatterMost IL2`, description: `IL2 Chat platform`, url: `https://chat.il2.dso.mil/`, icon_url: `/icons/iconName.svg`, role: '/Platform One/Party Bus/IL2/IL2-Atlassian' },
+    { name: `Jira`, description: ``, url: ``, icon_url: ``, role: '/Platform One/Party Bus/IL2/IL2-Atlassian' },
+    { name: `Confluence`, description: ``, url: ``, icon_url: ``, role: '/Platform One/Party Bus/IL2/IL2-Atlassian' },
+    { name: `GitLab`, description: `Gitlab for Platform One`, url: ``, icon_url: ``, role: '/Platform One/Party Bus/IL2/IL2-Gitlab' },
+    { name: ``, description: ``, url: ``, icon_url: ``, role: '' },
 
-//   function handleAppSubmit - function that runs when product team submits a new app
-//        handles changes to appList
-//   function handleIronBankSearchSubmit - function once search is entered
-//        handles a change to search item (need state for search item?)
-//   function handleRepoSearch - function for searching repo1 (navbar searchbar)\
-//   function handleCategorySearch - function for filtering by category
+  ]);
+  //full list of Apps in database (this is an array of objects of all apps)
+  //   const [activeApps, setActiveApps] = useState([]) //set based on token on load (this is an array including each app object that is active)
+  //   const [isAdmin, setIsAdmin] = useState([])
+  //   const [searchTerm, setSearchTerm] = useState([])
+  // a favorites or MyApp state?
 
-// if we need these, redo these add and remove functions for this project (this was using two diff arrays, Favorites and isFav)
-//   function addFavorite(appObj){
-//       if (!favorites.includes(appObj)) {
-//           setFavorites(favorites.concat(appObj));
-//           setIsFav(isFav.concat(appObj.name));
-//       } 
-//   }
+  //   function handleAppSubmit - function that runs when product team submits a new app
+  //        handles changes to appList
+  //   function handleIronBankSearchSubmit - function once search is entered
+  //        handles a change to search item (need state for search item?)
+  //   function handleRepoSearch - function for searching repo1 (navbar searchbar)\
+  //   function handleCategorySearch - function for filtering by category
 
-//   function removeFavorite(appObj){
-//       let index = favorites.indexOf(appObj);
-//       let temp = [...favorites.slice(0, index), ...favorites.slice(index + 1)];
-//       setFavorites(temp);
-//       let index2 = isFav.indexOf(appObj.name);
-//       let temp2 = [...isFav.slice(0, index2), ...isFav.slice(index2 + 1)];
-//       setIsFav(temp2);
-//   }
+  // if we need these, redo these add and remove functions for this project (this was using two diff arrays, Favorites and isFav)
+  //   function addFavorite(appObj){
+  //       if (!favorites.includes(appObj)) {
+  //           setFavorites(favorites.concat(appObj));
+  //           setIsFav(isFav.concat(appObj.name));
+  //       }
+  //   }
 
-//   function getJSON(res) {
-//       return res.json();
-//   }
+  //   function removeFavorite(appObj){
+  //       let index = favorites.indexOf(appObj);
+  //       let temp = [...favorites.slice(0, index), ...favorites.slice(index + 1)];
+  //       setFavorites(temp);
+  //       let index2 = isFav.indexOf(appObj.name);
+  //       let temp2 = [...isFav.slice(0, index2), ...isFav.slice(index2 + 1)];
+  //       setIsFav(temp2);
+  //   }
 
-//   useEffect(() => {
-//       fetch API
-//       set list
-//       compare with token
-//       set state active Apps
-//       sort apps by active and inactive (make array of active using the token group-full, make array of inactive on the rest, then use .concat to recombine into one array)
-//       set state inactive Apps (logic to set only inactive apps. Based on jwt group -full or simple)
-//   }, []);
+  //   function getJSON(res) {
+  //       return res.json();
+  //   }
 
-// create object with all your states and state-changing functions for the children to use
+  //   useEffect(() => {
+  //       fetch API
+  //       set list
+  //       compare with token
+  //       set state active Apps
+  //       sort apps by active and inactive (make array of active using the token group-full, make array of inactive on the rest, then use .concat to recombine into one array)
+  //       set state inactive Apps (logic to set only inactive apps. Based on jwt group -full or simple)
+  //   }, []);
+
+  // create object with all your states and state-changing functions for the children to use
   const valueObj = {
-//       list,
-//       setList,
-//       myApps,
-//       setMyApps,
-//       isMyApps,
-//       setIsMyApp,
-//       isActive,
-//       setIsActive,
-//       addMyApp, 
-//       removeMyApp,
+    //       list,
+    //       setList,
+    //       myApps,
+    //       setMyApps,
+    //       isMyApps,
+    //       setIsMyApp,
+    //       isActive,
+    //       setIsActive,
+    //       addMyApp,
+    //       removeMyApp,
   };
 
   return (
-      <div className="App-provider">
-          <AppContext.Provider value={valueObj}>
-              {children}
-          </AppContext.Provider>
-      </div>
+    <div className="App-provider">
+      <AppContext.Provider value={valueObj}>
+        {children}
+      </AppContext.Provider>
+    </div>
   );
 }
 
